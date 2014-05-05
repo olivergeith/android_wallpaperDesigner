7faf88834f5d771a35fd8203f7b62f9b5b7da6ea
Oliver <oliver.geith@gmx.net>
2014-05-05 20:16:09 +0200

    ################# Version 1.2

--- src/de/geithonline/wallpaperdesigner/settings/Settings.java
+++ src/de/geithonline/wallpaperdesigner/settings/Settings.java
@@ -108,23 +108,23 @@ public class Settings {
 	public static int getWidth() {
 		final String s = getSizeSelection();
 		switch (s) {
-			case "customSize":
-				return getBWidth();
-			default:
-			case "2560x1600":
-				return 2560;
-			case "1920x1200":
-				return 1920;
-			case "1920x1080":
-				return 1920;
-			case "1280x720":
-				return 1280;
-			case "1024x768":
-				return 1024;
-			case "960x800":
-				return 960;
-			case "640x480":
-				return 640;
+		case "customSize":
+			return getBWidth();
+		default:
+		case "2560x1600":
+			return 2560;
+		case "1920x1200":
+			return 1920;
+		case "1920x1080":
+			return 1920;
+		case "1280x720":
+			return 1280;
+		case "1024x768":
+			return 1024;
+		case "960x800":
+			return 960;
+		case "640x480":
+			return 640;
 		}
 	}
 
@@ -139,23 +139,23 @@ public class Settings {
 	public static int getHeight() {
 		final String s = getSizeSelection();
 		switch (s) {
-			case "customSize":
-				return getBHeight();
-			default:
-			case "2560x1600":
-				return 1600;
-			case "1920x1200":
-				return 1200;
-			case "1920x1080":
-				return 1080;
-			case "1280x720":
-				return 720;
-			case "1024x768":
-				return 768;
-			case "960x800":
-				return 800;
-			case "640x480":
-				return 480;
+		case "customSize":
+			return getBHeight();
+		default:
+		case "2560x1600":
+			return 1600;
+		case "1920x1200":
+			return 1200;
+		case "1920x1080":
+			return 1080;
+		case "1280x720":
+			return 720;
+		case "1024x768":
+			return 768;
+		case "960x800":
+			return 800;
+		case "640x480":
+			return 480;
 		}
 	}
 
@@ -269,7 +269,7 @@ public class Settings {
 
 			prefs.edit().putBoolean("dropShadow", true).commit();
 			prefs.edit().putBoolean("randomizeDropShadowColor", true).commit();
-			prefs.edit().putInt("colorDropShadow", R.integer.COLOR_BLACK).commit();
+			prefs.edit().putInt("colorDropShadow", Color.BLACK).commit();
 
 			prefs.edit().putString("bWidth", "2560").commit();
 			prefs.edit().putString("bHeight", "1600").commit();
