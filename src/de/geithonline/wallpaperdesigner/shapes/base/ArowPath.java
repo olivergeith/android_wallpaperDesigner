fa40f6ee4926455bef3fb5703353a77d479004f5
olivergeith <oliver.geith@gmx.net>
2014-10-13 12:23:55 +0200

    ArrowPath

---
+++ src/de/geithonline/wallpaperdesigner/shapes/base/ArowPath.java
@@ -0,0 +1,35 @@
+package de.geithonline.wallpaperdesigner.shapes.base;
+
+import android.graphics.Path;
+import android.graphics.PointF;
+
+public class ArowPath extends Path {
+
+	public ArowPath(final PointF center, final float radius, final Direction dir) {
+		draw(center, radius, dir);
+	}
+
+	private void draw(final PointF center, final float radius, final Direction dir) {
+		final float raster = radius / 3;
+		if (dir.equals(Direction.CW)) {
+			moveTo(center.x + 0 * raster, center.y - 3 * raster);
+			lineTo(center.x + 3 * raster, center.y + 1 * raster);
+			lineTo(center.x + 1 * raster, center.y + 1 * raster);
+			lineTo(center.x + 1 * raster, center.y + 3 * raster);
+			lineTo(center.x - 1 * raster, center.y + 3 * raster);
+			lineTo(center.x - 1 * raster, center.y + 1 * raster);
+			lineTo(center.x - 3 * raster, center.y + 1 * raster);
+			close();
+		} else {
+			moveTo(center.x + 0 * raster, center.y - 3 * raster);
+			lineTo(center.x - 3 * raster, center.y + 1 * raster);
+			lineTo(center.x - 1 * raster, center.y + 1 * raster);
+			lineTo(center.x - 1 * raster, center.y + 3 * raster);
+			lineTo(center.x + 1 * raster, center.y + 3 * raster);
+			lineTo(center.x + 1 * raster, center.y + 1 * raster);
+			lineTo(center.x + 3 * raster, center.y + 1 * raster);
+			close();
+		}
+
+	}
+}
