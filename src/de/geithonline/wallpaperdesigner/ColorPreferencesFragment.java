18eea4607ec41db2914a3e17a66c1c8e9a522aac
Oliver <oliver.geith@gmx.net>
2014-12-31 13:39:36 +0100

    Preview for Color Layout

--- src/de/geithonline/wallpaperdesigner/ColorPreferencesFragment.java
+++ src/de/geithonline/wallpaperdesigner/ColorPreferencesFragment.java
@@ -1,17 +1,26 @@
 package de.geithonline.wallpaperdesigner;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import de.geithonline.android.basics.preferences.IconOnlyPreference;
 import de.geithonline.android.basics.preferences.colorpicker.ColorPickerPreference;
+import de.geithonline.wallpaperdesigner.bitmapdrawer.BackgroundDrawer;
 import de.geithonline.wallpaperdesigner.settings.Settings;
+import de.geithonline.wallpaperdesigner.utils.DisplayHelper;
 
 /**
  * This fragment shows the preferences for the first header.
  */
-public class ColorPreferencesFragment extends PreferenceFragment {
+public class ColorPreferencesFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
 
 	private ListPreference gradientDirection;
 	private ListPreference anzColors;
@@ -19,11 +28,25 @@ public class ColorPreferencesFragment extends PreferenceFragment {
 	private ColorPickerPreference color2;
 	private ColorPickerPreference color3;
 	private ColorPickerPreference color4;
+	private IconOnlyPreference colorPreview;
+	private Bitmap bitmap;
+
+	@Override
+	public void onDestroy() {
+		if (bitmap != null) {
+			bitmap.recycle();
+		}
+		super.onDestroy();
+	}
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences_color);
+
+		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity()
+				.getApplicationContext());
+		prefs.registerOnSharedPreferenceChangeListener(this);
 
 		color1 = (ColorPickerPreference) findPreference("color_plain_bgrnd");
 		color2 = (ColorPickerPreference) findPreference("color2_plain_bgrnd");
@@ -36,8 +59,9 @@ public class ColorPreferencesFragment extends PreferenceFragment {
 		color4.setHexValueEnabled(Settings.isHexValueEnabled());
 
 		gradientDirection = (ListPreference) findPreference("gradientDirection");
-		anzColors = (ListPreference) findPreference("anzColors");
+		colorPreview = (IconOnlyPreference) findPreference("colorPreview");
 
+		anzColors = (ListPreference) findPreference("anzColors");
 		anzColors.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 
 			@Override
@@ -56,6 +80,31 @@ public class ColorPreferencesFragment extends PreferenceFragment {
 			}
 		});
 		handleSelection(Settings.getGradientDirection(), Settings.getAnzahlGradientColors());
+		drawBackGroundImage();
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key) {
+		Log.i("ColorPreferenceFragment", "onPreference Change for " + key);
+		drawBackGroundImage();
+	}
+
+	private void drawBackGroundImage() {
+		Log.i("ColorPreferenceFragment", "drawing BackgroundIcon ");
+		final int w = Settings.getBWidth();
+		final int h = Settings.getBHeight();
+		final int bWidth = DisplayHelper.getDisplayWidth(getActivity()) / 2;
+		final int bHeight = (bWidth * h) / w;
+		bitmap = Bitmap.createBitmap(bWidth, bHeight, Bitmap.Config.ARGB_8888);
+		final Canvas bitmapCanvas = new Canvas(bitmap);
+		BackgroundDrawer.drawBackground(bitmapCanvas);
+		colorPreview.setTitle("Preview");
+		// final Drawable icon = BitmapHelper.bitmapToIcon(bitmap);
+		if (bitmap != null) {
+			colorPreview.setImage(bitmap);
+		} else {
+			Log.i("ColorPreferenceFragment", "drawing BackgroundIcon -Bitmap was null!!");
+		}
 	}
 
 	private void handleSelection(final String selection, final int anzahl) {
@@ -66,7 +115,7 @@ public class ColorPreferencesFragment extends PreferenceFragment {
 			enableColors(4);
 			anzColors.setSummary("" + 4);
 		} else {
-			enableColors(Settings.getAnzahlGradientColors());
+			enableColors(anzahl);
 		}
 	}
 
