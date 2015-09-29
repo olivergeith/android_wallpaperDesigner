3710cab2c41082e116983a6d583b6fb70197cd41
olivergeith <oliver.geith@gmx.net>
2015-09-29 10:45:55 +0200

    Removing loginfos

--- src/de/geithonline/wallpaperdesigner/bitmapdrawer/raster/CircularRaster.java
+++ src/de/geithonline/wallpaperdesigner/bitmapdrawer/raster/CircularRaster.java
@@ -1,7 +1,6 @@
 package de.geithonline.wallpaperdesigner.bitmapdrawer.raster;
 
 import android.graphics.Point;
-import android.util.Log;
 import de.geithonline.wallpaperdesigner.settings.Settings;
 import de.geithonline.wallpaperdesigner.utils.Randomizer;
 
@@ -39,21 +38,19 @@ public class CircularRaster extends AbstractRaster {
 
 		final Point center = new Point(width / 2, height);
 		points.add(center);
-		Log.i("Half Ring RASTER", "Anzahl Ringe = " + anzRinge);
+		// Log.i("Half Ring RASTER", "Anzahl Ringe = " + anzRinge);
 		for (int ring = 1; ring <= anzRinge; ring++) {
 			final float r = ring * radiusStep;
 			final int ecken = (int) (Math.PI * 2 * r) / radiusStep;
 			final float winkelProEcke = (float) (Math.PI / ecken) * 2;
-			Log.i("Half Ring RASTER", "Anzahl Rcken = " + ecken);
+			// Log.i("Half Ring RASTER", "Anzahl Rcken = " + ecken);
 			float startWinkel = (float) (Math.PI / 2);
 			if (Settings.isRandomStartwinkel()) {
 				startWinkel = Randomizer.getRandomFloat(0, (float) Math.PI * 2);
 			}
 			// final float startWinkel = (float) (Math.PI / 2);
 			for (int ecke = 0; ecke < ecken; ecke++) {
-
 				final Point p = new Point();
-
 				p.x = (int) (center.x + Math.cos(ecke * winkelProEcke + startWinkel) * r);
 				p.y = (int) (center.y + Math.sin(ecke * winkelProEcke + startWinkel) * r);
 				// Limitieren auf punkte innerhalb des Canvas
@@ -70,20 +67,18 @@ public class CircularRaster extends AbstractRaster {
 
 		final Point center = new Point(width / 2, height / 2);
 		points.add(center);
-		Log.i("CIRCULAR RASTER", "Anzahl Ringe = " + anzRinge);
+		// Log.i("CIRCULAR RASTER", "Anzahl Ringe = " + anzRinge);
 		for (int ring = 1; ring <= anzRinge; ring++) {
 			final float r = ring * radiusStep;
 			final int ecken = (int) (Math.PI * 2 * r) / radiusStep;
 			final float winkelProEcke = (float) (Math.PI / ecken) * 2;
-			Log.i("CIRCULAR RASTER", "Anzahl Ecken = " + ecken);
+			// Log.i("CIRCULAR RASTER", "Anzahl Ecken = " + ecken);
 			float startWinkel = (float) (Math.PI / 2);
 			if (Settings.isRandomStartwinkel()) {
 				startWinkel = Randomizer.getRandomFloat(0, (float) Math.PI * 2);
 			}
 			for (int ecke = 0; ecke < ecken; ecke++) {
-
 				final Point p = new Point();
-
 				p.x = (int) (center.x + Math.cos(ecke * winkelProEcke + startWinkel) * r);
 				p.y = (int) (center.y + Math.sin(ecke * winkelProEcke + startWinkel) * r);
 				// Limitieren auf punkte innerhalb des Canvas
@@ -98,19 +93,16 @@ public class CircularRaster extends AbstractRaster {
 		final int anzRinge = maximumRadius / radiusStep;
 		final Point center = new Point(width / 2, height / 2);
 		points.add(center);
-		Log.i("CIRCULAR RASTER", "Anzahl Ringe = " + anzRinge);
+		// Log.i("CIRCULAR RASTER", "Anzahl Ringe = " + anzRinge);
 
 		for (int ring = 0; ring <= anzRinge; ring++) {
 			final float r = ring * radiusStep;
 			final int ecken = (int) (Math.PI * 2 * r) / radiusStep + Math.max(anzRinge - ring * ring, 0);
 			final float winkelProEcke = (float) (Math.PI * 2 / (ecken));
-			Log.i("CIRCULAR RASTER", "Anzahl Rcken = " + ecken);
+			// Log.i("CIRCULAR RASTER", "Anzahl Punkte = " + ecken);
 			for (int ecke = 0; ecke < ecken; ecke++) {
-
 				final float rp = r + radiusStep * ecke / ecken;
-
 				final Point p = new Point();
-
 				p.x = (int) (center.x + Math.cos(ecke * winkelProEcke) * rp);
 				p.y = (int) (center.y + Math.sin(ecke * winkelProEcke) * rp);
 				// Limitieren auf punkte innerhalb des Canvas
