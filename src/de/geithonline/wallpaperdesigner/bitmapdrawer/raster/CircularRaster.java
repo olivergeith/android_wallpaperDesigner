4ccff409a2025c4c5177508740457d5d8b66ead7
Oliver <oliver.geith@gmx.net>
2015-11-13 07:58:28 +0100

    circulare raster vereinheitlicht

--- src/de/geithonline/wallpaperdesigner/bitmapdrawer/raster/CircularRaster.java
+++ src/de/geithonline/wallpaperdesigner/bitmapdrawer/raster/CircularRaster.java
@@ -1,7 +1,9 @@
 package de.geithonline.wallpaperdesigner.bitmapdrawer.raster;
 
 import android.graphics.Point;
+import android.graphics.PointF;
 import de.geithonline.wallpaperdesigner.settings.Settings;
+import de.geithonline.wallpaperdesigner.utils.GeometrieHelper;
 import de.geithonline.wallpaperdesigner.utils.Randomizer;
 
 public class CircularRaster extends AbstractRaster {
@@ -31,13 +33,40 @@ public class CircularRaster extends AbstractRaster {
 	}
 
 	private void calculateHalfRings(final int width, final int height, final int radius, final float overlap) {
-		final int maximumRadius = (int) Math.sqrt(width * width / 4 + height * height) + WIDE_CANVAS_LIMIT * getAbstand();
+		final PointF center = new PointF(width / 2, height);
+		calculateRings(width, height, radius, overlap, center);
+
+	}
+
+	private void calculateCenteredRingsRandomStartWinkel(final int width, final int height, final int radius, final float overlap) {
+		final PointF center = new PointF(width / 2, height / 2);
+		calculateRings(width, height, radius, overlap, center);
+
+	}
+
+	private void calculateRings(final int width, final int height, final int radius, final float overlap, final PointF center) {
+		float maximumRadius = 0;
+		float max = GeometrieHelper.calcDistance(center, new PointF(0f, 0f)) + WIDE_CANVAS_LIMIT * getAbstand();
+		if (max > maximumRadius) {
+			maximumRadius = max;
+		}
+		max = GeometrieHelper.calcDistance(center, new PointF(width, 0f)) + WIDE_CANVAS_LIMIT * getAbstand();
+		if (max > maximumRadius) {
+			maximumRadius = max;
+		}
+		max = GeometrieHelper.calcDistance(center, new PointF(0f, height)) + WIDE_CANVAS_LIMIT * getAbstand();
+		if (max > maximumRadius) {
+			maximumRadius = max;
+		}
+		max = GeometrieHelper.calcDistance(center, new PointF(width, height)) + WIDE_CANVAS_LIMIT * getAbstand();
+		if (max > maximumRadius) {
+			maximumRadius = max;
+		}
 
 		final int radiusStep = Math.round(radius * 2 * overlap);
-		final int anzRinge = maximumRadius / radiusStep + 2;
+		final int anzRinge = (int) (maximumRadius / radiusStep + 2);
 
-		final Point center = new Point(width / 2, height);
-		points.add(center);
+		points.add(new Point((int) center.x, (int) center.y));
 		// Log.i("Half Ring RASTER", "Anzahl Ringe = " + anzRinge);
 		for (int ring = 1; ring <= anzRinge; ring++) {
 			final float r = ring * radiusStep;
@@ -49,34 +78,6 @@ public class CircularRaster extends AbstractRaster {
 				startWinkel = Randomizer.getRandomFloat(0, (float) Math.PI * 2);
 			}
 			// final float startWinkel = (float) (Math.PI / 2);
-			for (int ecke = 0; ecke < ecken; ecke++) {
-				final Point p = new Point();
-				p.x = (int) (center.x + Math.cos(ecke * winkelProEcke + startWinkel) * r);
-				p.y = (int) (center.y + Math.sin(ecke * winkelProEcke + startWinkel) * r);
-				// Limitieren auf punkte innerhalb des Canvas
-				addPoint2List(width, height, p);
-			}
-		}
-	}
-
-	private void calculateCenteredRingsRandomStartWinkel(final int width, final int height, final int radius, final float overlap) {
-		final int maximumRadius = (int) Math.sqrt(width * width / 4 + height * height / 4) + WIDE_CANVAS_LIMIT * getAbstand();
-
-		final int radiusStep = Math.round(radius * 2 * overlap);
-		final int anzRinge = maximumRadius / radiusStep + 1;
-
-		final Point center = new Point(width / 2, height / 2);
-		points.add(center);
-		// Log.i("CIRCULAR RASTER", "Anzahl Ringe = " + anzRinge);
-		for (int ring = 1; ring <= anzRinge; ring++) {
-			final float r = ring * radiusStep;
-			final int ecken = (int) (Math.PI * 2 * r) / radiusStep;
-			final float winkelProEcke = (float) (Math.PI / ecken) * 2;
-			// Log.i("CIRCULAR RASTER", "Anzahl Ecken = " + ecken);
-			float startWinkel = (float) (Math.PI / 2);
-			if (Settings.isRandomStartwinkel()) {
-				startWinkel = Randomizer.getRandomFloat(0, (float) Math.PI * 2);
-			}
 			for (int ecke = 0; ecke < ecken; ecke++) {
 				final Point p = new Point();
 				p.x = (int) (center.x + Math.cos(ecke * winkelProEcke + startWinkel) * r);
