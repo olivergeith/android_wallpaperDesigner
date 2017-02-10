3e4e844ef9e29e9fd25ecbc42ff57caba2f5be60
olivergeith <oliver.geith@gmx.net>
2017-02-10 10:35:04 +0100

    radius calculator

--- src/de/geithonline/wallpaperdesigner/bitmapdrawer/patterndrawer/RadiusCalculator.java
+++ src/de/geithonline/wallpaperdesigner/bitmapdrawer/patterndrawer/RadiusCalculator.java
@@ -1,5 +1,6 @@
 package de.geithonline.wallpaperdesigner.bitmapdrawer.patterndrawer;
 
+import de.geithonline.wallpaperdesigner.settings.Settings;
 import de.geithonline.wallpaperdesigner.utils.Randomizer;
 
 public class RadiusCalculator {
@@ -8,6 +9,9 @@ public class RadiusCalculator {
     private final int minRadius;
     private final int maxRadius;
     private final int delta;
+    private final int blurLevel1;
+    private final int blurLevel2;
+    private final int blurLevel3;
 
     private enum RADIUS_TYPE {
         random, rising, dependingOnBlurrStage;
@@ -31,6 +35,11 @@ public class RadiusCalculator {
         this.minRadius = minRadius;
         this.maxRadius = maxRadius;
         delta = maxRadius - minRadius;
+        blurLevel1 = anzPattern * Settings.getBlurrStage1() / 100;
+        blurLevel2 = anzPattern * Settings.getBlurrStage2() / 100;
+        blurLevel3 = anzPattern * Settings.getBlurrStage3() / 100;
+        final int size1 = minRadius;
+        final int size2 = 0;
     }
 
     public int getSize(final int index) {
@@ -41,6 +50,7 @@ public class RadiusCalculator {
         case rising:
             return minRadius + delta * index / anzPattern;
         case dependingOnBlurrStage:
+            // if(index < 1)
             return Randomizer.getRandomInt(minRadius, maxRadius);
         }
     }
