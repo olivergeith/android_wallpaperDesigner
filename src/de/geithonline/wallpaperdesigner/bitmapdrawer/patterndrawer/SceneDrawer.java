29500d084eaf0d42bec46a4bf1e74e88a07a769c
Oliver <oliver.geith@gmx.net>
2017-06-22 19:36:38 +0200

    Sine Trail

--- src/de/geithonline/wallpaperdesigner/bitmapdrawer/patterndrawer/SceneDrawer.java
+++ src/de/geithonline/wallpaperdesigner/bitmapdrawer/patterndrawer/SceneDrawer.java
@@ -13,34 +13,31 @@ import de.geithonline.wallpaperdesigner.utils.PathHelper;
 
 public class SceneDrawer {
 
-    private final Canvas bitmapCanvas;
-    private final GlossyDrawer glossyDrawer;
-    private final OutlineDrawer outlineDrawer;
-    private final Rotator rotator;
+	private final Canvas bitmapCanvas;
+	private final GlossyDrawer glossyDrawer;
+	private final OutlineDrawer outlineDrawer;
+	private final Rotator rotator;
 
-    public SceneDrawer(final Canvas bitmapCanvas, final GlossyDrawer glossyDrawer, final OutlineDrawer outlineDrawer,
-            final Rotator rotator) {
-        this.bitmapCanvas = bitmapCanvas;
-        this.glossyDrawer = glossyDrawer;
-        this.outlineDrawer = outlineDrawer;
-        this.rotator = rotator;
+	public SceneDrawer(final Canvas bitmapCanvas, final GlossyDrawer glossyDrawer, final OutlineDrawer outlineDrawer, final Rotator rotator) {
+		this.bitmapCanvas = bitmapCanvas;
+		this.glossyDrawer = glossyDrawer;
+		this.outlineDrawer = outlineDrawer;
+		this.rotator = rotator;
 
-    }
+	}
 
-    public void drawStarWithTrail(final int x, final int y, final Paint paint, final int radius, final boolean filled,
-            final TRAIL_TYPE type) {
-        final StarTrailPath path = new StarTrailPath(new PointF(x, y), radius, filled, type);
-        PathHelper.rotatePath(x, y, path, rotator.getRotationDegrees(0, 360, new Point(x, y)));
-        // PathHelper.rotatePath(x, y, path.trailPath,
-        // rotator.getRotationDegrees(0, 360, new Point(x, y)));
+	public void drawStarWithTrail(final int x, final int y, final Paint paint, final int radius, final boolean filled, final TRAIL_TYPE type) {
+		final StarTrailPath path = new StarTrailPath(new PointF(x, y), radius, filled, type);
+		PathHelper.rotatePath(x, y, path, rotator.getRotationDegrees(0, 360, new Point(x, y)));
+		// PathHelper.rotatePath(x, y, path.trailPath, rotator.getRotationDegrees(0, 360, new Point(x, y)));
 
-        // trail
-        glossyDrawer.draw(x, y, paint, radius, path.trailPath, //
-                GLOSSY_REFLECTIONS_STYLE.NONE, GLOSSY_GLOW_STYLE.VERTICAL, true);
-        // star
-        bitmapCanvas.drawPath(path, paint);
-        glossyDrawer.draw(x, y, paint, radius, path);
-        outlineDrawer.draw(paint, radius, path);
-    }
+		// star
+		bitmapCanvas.drawPath(path, paint);
+		glossyDrawer.draw(x, y, paint, radius, path);
+		outlineDrawer.draw(paint, radius, path);
+		// trail
+		glossyDrawer.draw(x, y, paint, radius, path.trailPath, //
+				GLOSSY_REFLECTIONS_STYLE.NONE, GLOSSY_GLOW_STYLE.VERTICAL, true);
+	}
 
 }
--- src/de/geithonline/wallpaperdesigner/shapes/StarTrailPath.java
+++ src/de/geithonline/wallpaperdesigner/shapes/StarTrailPath.java
@@ -8,91 +8,113 @@ import de.geithonline.wallpaperdesigner.utils.Randomizer;
 
 public class StarTrailPath extends Path {
 
-    public enum TRAIL_TYPE {
-        Stars, Lines, StarsGettingBigger;
-    }
+	public enum TRAIL_TYPE {
+		Stars, Lines, StarsGettingBigger, Sinus;
+	}
 
-    public Path trailPath = new Path();
+	public Path trailPath = new Path();
 
-    public StarTrailPath(final PointF center, final float radius, final boolean filled, final TRAIL_TYPE type) {
-        super();
+	public StarTrailPath(final PointF center, final float radius, final boolean filled, final TRAIL_TYPE type) {
+		super();
 
-        addPath(new StarPath(5, center, radius, STAR_TYPE.NORMAL, true));
-        addPath(new KochSnowFlakePath(center, radius, true, 3));
-        switch (type) {
-            default:
-            case Stars:
-                createTrailOfStars(center, radius, filled);
-                break;
-            case StarsGettingBigger:
-                createTrailOfStarsGettingBigger(center, radius, filled);
-                break;
-            case Lines:
-                createTrailOfLines(center, radius, filled);
-                break;
-        }
+		addPath(new StarPath(5, center, radius, STAR_TYPE.NORMAL, true));
+		// addPath(new KochSnowFlakePath(center, radius, true, 3));
+		if (filled) {
+			// if (Randomizer.getRandomInt(0, 5) == 1) {
+			switch (type) {
+			default:
+			case Stars:
+				createTrailOfStars(center, radius, filled);
+				break;
+			case Sinus:
+				createSinusTrailOfStars(center, radius, filled);
+				break;
+			case StarsGettingBigger:
+				createTrailOfStarsGettingBigger(center, radius, filled);
+				break;
+			case Lines:
+				createTrailOfLines(center, radius, filled);
+				break;
+			}
+		}
 
-    }
+	}
 
-    private void createTrailOfStars(final PointF center, final float radius, final boolean filled) {
+	private void createTrailOfStars(final PointF center, final float radius, final boolean filled) {
 
-        // Trail ony when filled
-        if (filled) {
-            final float distanceToTop = center.y - 5;
-            final float trailRadius = radius / 5;
-            final float distance = 2f;
-            final int numberOfTrailStars = (int) (distanceToTop / trailRadius / distance);
-            for (int i = 0; i < numberOfTrailStars - 1; i++) {
-                final PointF pos = new PointF();
-                pos.x = center.x + Randomizer.getRandomFloat(-trailRadius / 2, +trailRadius / 2);
-                pos.y = 5 + distance * trailRadius * i;
-                final float randRadius = Randomizer.getRandomFloat(trailRadius / 2, trailRadius * 2);
-                if (Randomizer.getRandomInt(0, 5) != 1) {
-                    trailPath.addPath(new StarPath(5, pos, randRadius, STAR_TYPE.NORMAL, true));
-                }
-            }
-        }
-    }
+		// Trail ony when filled
+		final float distanceToTop = center.y - 5;
+		final float trailRadius = radius / 5;
+		final float distance = 2f;
+		final int numberOfTrailStars = (int) (distanceToTop / trailRadius / distance);
+		for (int i = 0; i < numberOfTrailStars - 1; i++) {
+			final PointF pos = new PointF();
+			pos.x = center.x + Randomizer.getRandomFloat(-trailRadius / 2, +trailRadius / 2);
+			pos.y = 5 + distance * trailRadius * i;
+			final float randRadius = Randomizer.getRandomFloat(trailRadius / 2, trailRadius * 2);
+			if (Randomizer.getRandomInt(0, 5) != 1) {
+				trailPath.addPath(new StarPath(5, pos, randRadius, STAR_TYPE.NORMAL, true));
+			}
+		}
+	}
 
-    private void createTrailOfStarsGettingBigger(final PointF center, final float radius, final boolean filled) {
+	private void createSinusTrailOfStars(final PointF center, final float radius, final boolean filled) {
 
-        // Trail ony when filled
-        if (filled) {
-            final float distanceToTop = center.y - 5;
-            final float trailRadius = radius / 5;
-            final float distance = 2.0f;
-            final int numberOfTrailStars = (int) (distanceToTop / trailRadius / distance);
-            for (int i = 0; i < numberOfTrailStars - 1; i++) {
-                final PointF pos = new PointF();
-                pos.x = center.x + Randomizer.getRandomFloat(-trailRadius * 1.5f, +trailRadius * 1.5f);
-                pos.y = 5 + distance * trailRadius * i;
-                final float starRadius = trailRadius * 0.3f //
-                        + (float) i / (float) numberOfTrailStars * 0.7f * trailRadius//
-                        + Randomizer.getRandomFloat(0, 0.7f * trailRadius);
+		// Trail ony when filled
+		final float distanceToTop = center.y - 5;
+		final float trailRadius = radius / 5;
+		final float distance = 2f;
+		final int numberOfTrailStars = (int) (distanceToTop / trailRadius / distance);
+		final int sinRepeats = numberOfTrailStars / 5;
+		for (int i = 0; i < numberOfTrailStars - 1; i++) {
+			final PointF pos = new PointF();
+			final float angle = (float) ((float) i / numberOfTrailStars * Math.PI * sinRepeats);
+			pos.x = center.x + (float) (trailRadius * 2 * Math.sin(angle));// Randomizer.getRandomFloat(-trailRadius / 2, +trailRadius / 2);
+			pos.y = 5 + distance * trailRadius * i;
+			final float randRadius = Randomizer.getRandomFloat(trailRadius / 2, trailRadius * 2);
+			if (Randomizer.getRandomInt(0, 5) != 1) {
+				trailPath.addPath(new StarPath(5, pos, randRadius, STAR_TYPE.NORMAL, true));
+			}
+		}
+	}
 
-                if (Randomizer.getRandomInt(0, 6) != 1) {
-                    trailPath.addPath(new StarPath(5, pos, starRadius, STAR_TYPE.NORMAL, true));
-                }
-            }
-        }
-    }
+	private void createTrailOfStarsGettingBigger(final PointF center, final float radius, final boolean filled) {
 
-    private void createTrailOfLines(final PointF center, final float radius, final boolean filled) {
+		// Trail ony when filled
+		final float distanceToTop = center.y - 5;
+		final float trailRadius = radius / 5;
+		final float distance = 2.0f;
+		final int numberOfTrailStars = (int) (distanceToTop / trailRadius / distance);
+		for (int i = 0; i < numberOfTrailStars - 1; i++) {
+			final PointF pos = new PointF();
+			pos.x = center.x + Randomizer.getRandomFloat(-trailRadius * 1.5f, +trailRadius * 1.5f);
+			pos.y = 5 + distance * trailRadius * i;
+			final float starRadius = trailRadius * 0.3f //
+					+ (float) i / (float) numberOfTrailStars * 0.7f * trailRadius//
+					+ Randomizer.getRandomFloat(0, 0.7f * trailRadius);
 
-        // Trail ony when filled
-        if (filled) {
-            final float distanceToTop = center.y - 5;
-            final float trailRadius = radius / 3;
-            final float distance = 0.9f;
-            final int numberOfTrailStars = (int) (distanceToTop / trailRadius / distance);
-            for (int i = 0; i < numberOfTrailStars - 2; i++) {
-                final PointF pos = new PointF();
-                pos.x = center.x + Randomizer.getRandomFloat(-trailRadius * 0.75f, +trailRadius * 0.75f);
-                pos.y = 5 + distance * trailRadius * i;
-                final float randY = Randomizer.getRandomFloat(trailRadius * 2, 5 * trailRadius);
-                trailPath.addPath(new OvalPath(pos, radius / 30, randY, Direction.CW));
-            }
-        }
-    }
+			if (Randomizer.getRandomInt(0, 6) != 1) {
+				trailPath.addPath(new StarPath(5, pos, starRadius, STAR_TYPE.NORMAL, true));
+			}
+		}
+	}
+
+	private void createTrailOfLines(final PointF center, final float radius, final boolean filled) {
+
+		// Trail ony when filled
+		final float distanceToTop = center.y - 5;
+		final float trailRadius = radius / 3;
+		final float distance = 0.9f;
+		final int numberOfTrailStars = (int) (distanceToTop / trailRadius / distance);
+		for (int i = 0; i < numberOfTrailStars - 3; i++) {
+			final PointF pos = new PointF();
+			// final float angle = (float) ((float) i / numberOfTrailStars * Math.PI * sinRepeats);
+			// pos.x = center.x + (float) (trailRadius * 2 * Math.sin(angle));// Randomizer.getRandomFloat(-trailRadius / 2, +trailRadius / 2);
+			pos.x = center.x + Randomizer.getRandomFloat(-trailRadius * 0.75f, +trailRadius * 0.75f);
+			pos.y = 5 + distance * trailRadius * i;
+			final float randY = Randomizer.getRandomFloat(trailRadius * 2, 4 * trailRadius);
+			trailPath.addPath(new OvalPath(pos, radius / 30, randY, Direction.CW));
+		}
+	}
 
 }
