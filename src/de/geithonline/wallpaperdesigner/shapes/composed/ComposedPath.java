139e8ce7f7b4ce521031da4a1b728bc885993b4e
olivergeith <oliver.geith@gmx.net>
2017-06-29 08:57:58 +0200

    Composed Path refactored

--- src/de/geithonline/wallpaperdesigner/shapes/composed/ComposedPath.java
+++ src/de/geithonline/wallpaperdesigner/shapes/composed/ComposedPath.java
@@ -1,27 +1,33 @@
 
 package de.geithonline.wallpaperdesigner.shapes.composed;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import android.graphics.Path;
 
 public class ComposedPath extends Path {
 
-    private final List<Path> pathElements;
+    private final Map<String, Path> pathElements;
 
     public ComposedPath() {
-        pathElements = new ArrayList<>();
+        pathElements = new HashMap<>();
     }
 
     @Override
     public void addPath(final Path p) {
         super.addPath(p);
-        pathElements.add(p);
+        pathElements.put(UUID.randomUUID().toString(), p);
     }
 
-    public List<Path> getPathElements() {
-        return pathElements;
+    public Collection<Path> getPathElements() {
+        return pathElements.values();
+    }
+
+    public Path getPathElement(final String key) {
+        return pathElements.get(key);
     }
 
 }
