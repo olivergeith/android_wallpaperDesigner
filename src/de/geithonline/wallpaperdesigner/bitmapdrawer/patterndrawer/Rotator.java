ea19085f694e7c14678ea679f0e8b15322fb80e9
olivergeith <oliver.geith@gmx.net>
2018-04-19 12:40:54 +0200

    Rotator reworked

--- src/de/geithonline/wallpaperdesigner/bitmapdrawer/patterndrawer/Rotator.java
+++ src/de/geithonline/wallpaperdesigner/bitmapdrawer/patterndrawer/Rotator.java
@@ -1,122 +1,137 @@
 
 package de.geithonline.wallpaperdesigner.bitmapdrawer.patterndrawer;
 
-import android.graphics.Point;
 import android.graphics.PointF;
 import de.geithonline.wallpaperdesigner.settings.Settings;
 import de.geithonline.wallpaperdesigner.utils.Randomizer;
 
 public class Rotator {
 
-	private final int bWidth;
-	private final int bHeight;
+    private final int bWidth;
+    private final int bHeight;
 
-	private float incrementingDegrees = 0;
+    private float incrementingDegrees = 0;
 
-	public Rotator(final int bWidth, final int bHeight) {
-		this.bWidth = bWidth;
-		this.bHeight = bHeight;
-	}
+    public Rotator(final int bWidth, final int bHeight) {
+        this.bWidth = bWidth;
+        this.bHeight = bHeight;
+    }
 
-	public float getRotationDegrees(final int randomMin, final int randomMax, final Point center) {
-		switch (Settings.getRotationStyle()) {
-		default:
-		case "Fixed":
-			return Settings.getFixedRotationDegrees() + getRandom90Degrees() + getIncrementingDegrees();
-		case "Random":
-			return Randomizer.getRandomFloat(randomMin - 1, randomMax);
-		case "Random (Range)":
-			return Randomizer.getRandomFloat(Settings.getFixedRotationDegrees() - Settings.getrandomRangeDegrees(), //
-					Settings.getFixedRotationDegrees() + Settings.getrandomRangeDegrees()) + getRandom90Degrees() + getIncrementingDegrees();
+    public float getRotationDegrees(final int randomMin, final int randomMax, final PointF center) {
+        final PointF rotationCenter = new PointF(bWidth / 2f, bHeight / 2f);
 
-		case "Around Adjustable Point":
-		case "Around Adjustable Center": {
-			final float distTCenterX = bWidth * Settings.getRotationCenterPointX() - center.x;
-			final float distTCenterY = bHeight * Settings.getRotationCenterPointY() - center.y;
-			final float alpha = (float) Math.atan(distTCenterY / distTCenterX);
-			float winkel = (float) (alpha * 180 / Math.PI);
-			// Log.i("Winkel", "Winkel = " + winkel + "(" + alpha + ")");
-			if (center.x <= bWidth * Settings.getRotationCenterPointX()) {
-				winkel = winkel + 180;
-			}
-			return winkel + 90 + Settings.getFixedRotationDegrees() + getRandom90Degrees() + getIncrementingDegrees();
-		}
-		case "Around Center":
-		case "Around Point": {
-			final float distTCenterX = bWidth / 2 - center.x;
-			final float distTCenterY = bHeight / 2 - center.y;
-			final float alpha = (float) Math.atan(distTCenterY / distTCenterX);
-			float winkel = (float) (alpha * 180 / Math.PI);
-			// Log.i("Winkel", "Winkel = " + winkel + "(" + alpha + ")");
-			if (center.x <= bWidth / 2) {
-				winkel = winkel + 180;
-			}
-			return winkel + 90 + Settings.getFixedRotationDegrees() + getRandom90Degrees() + getIncrementingDegrees();
-		}
-		case "Around Point (Range)": {
-			final float distTCenterX = bWidth / 2 - center.x;
-			final float distTCenterY = bHeight / 2 - center.y;
-			final float alpha = (float) Math.atan(distTCenterY / distTCenterX);
-			float winkel = (float) (alpha * 180 / Math.PI);
-			// Log.i("Winkel", "Winkel = " + winkel + "(" + alpha + ")");
-			if (center.x <= bWidth / 2) {
-				winkel = winkel + 180;
-			}
-			final float rand = Randomizer.getRandomFloat(-Settings.getrandomRangeDegrees(), //
-					+Settings.getrandomRangeDegrees());
-			return winkel + 90 + Settings.getFixedRotationDegrees() + rand + getRandom90Degrees() + getIncrementingDegrees();
-		}
-		case "Around Bottom": {
-			final float distTCenterX = bWidth / 2 - center.x;
-			final float distTCenterY = bHeight - center.y;
-			final float alpha = (float) Math.atan(distTCenterY / distTCenterX);
-			float winkel = (float) (alpha * 180 / Math.PI);
-			// Log.i("Winkel", "Winkel = " + winkel + "(" + alpha + ")");
-			if (center.x <= bWidth / 2) {
-				winkel = winkel + 180;
-			}
-			return winkel + 90 + Settings.getFixedRotationDegrees() + getRandom90Degrees() + getIncrementingDegrees();
-		}
-		}
-	}
+        switch (Settings.getRotationStyle()) {
+            default:
+            case "Fixed":
+                return Settings.getFixedRotationDegrees() + getRandom90Degrees() + getIncrementingDegrees();
+            case "Random":
+                return Randomizer.getRandomFloat(randomMin - 1, randomMax);
+            case "Random (Range)":
+                return Randomizer.getRandomFloat(Settings.getFixedRotationDegrees() - Settings.getrandomRangeDegrees(), //
+                        Settings.getFixedRotationDegrees() + Settings.getrandomRangeDegrees()) + getRandom90Degrees() + getIncrementingDegrees();
 
-	public float getIncrementingDegrees() {
-		incrementingDegrees += Settings.getIncrementingDegreesAddingAmount();
-		if (incrementingDegrees > 180) {
-			incrementingDegrees = -180 + (incrementingDegrees - 180);
-		}
-		if (incrementingDegrees < -180) {
-			incrementingDegrees = 180 - (incrementingDegrees + 180);
-		}
-		return incrementingDegrees;
-	}
+            case "Around Adjustable Point":
+            case "Around Adjustable Center": {
+                rotationCenter.x = bWidth * Settings.getRotationCenterPointX();
+                rotationCenter.y = bHeight * Settings.getRotationCenterPointY();
+                final float winkel = calcWinkel(center, rotationCenter);
+                return winkel + 90 + Settings.getFixedRotationDegrees() + getRandom90Degrees() + getIncrementingDegrees();
+            }
+            case "Around Center":
+            case "Around Point": {
+                final float winkel = calcWinkel(center, rotationCenter);
+                return winkel + 90 + Settings.getFixedRotationDegrees() + getRandom90Degrees() + getIncrementingDegrees();
+            }
+            case "Around Point (Range)":
+            case "Around Center (Range)": {
+                final float winkel = calcWinkel(center, rotationCenter);
+                final float rand = Randomizer.getRandomFloat(-Settings.getrandomRangeDegrees(), //
+                        +Settings.getrandomRangeDegrees());
+                return winkel + 90 + Settings.getFixedRotationDegrees() + rand + getRandom90Degrees() + getIncrementingDegrees();
+            }
+            case "Around Bottom": {
+                rotationCenter.x = bWidth / 2f;
+                rotationCenter.y = bHeight;
+                final float winkel = calcWinkel(center, rotationCenter);
+                return winkel + 90 + Settings.getFixedRotationDegrees() + getRandom90Degrees() + getIncrementingDegrees();
+            }
+            case "Around Corners": {
+                if (center.x < bWidth / 2) {
+                    if (center.y < bHeight / 2) {
+                        rotationCenter.x = 0;
+                        rotationCenter.y = 0;
+                    } else {
+                        rotationCenter.x = 0;
+                        rotationCenter.y = bHeight;
+                    }
+                } else {
+                    if (center.y < bHeight / 2) {
+                        rotationCenter.x = bWidth;
+                        rotationCenter.y = 0;
+                    } else {
+                        rotationCenter.x = bWidth;
+                        rotationCenter.y = bHeight;
+                    }
+                }
+                final float winkel = calcWinkel(center, rotationCenter);
+                return winkel + 90 + Settings.getFixedRotationDegrees() + getRandom90Degrees() + getIncrementingDegrees();
+            }
+        }
+    }
 
-	public int getRandom90Degrees() {
-		if (Settings.isRandomDegreesAdding()) {
-			if (Randomizer.getRandomBoolean()) {
-				return Settings.getRandomDegreesAddingAmount();
-			}
-		}
-		return 0;
-	}
+    /**
+     * @param point
+     *            the point outside the center....
+     * @param rotationCenter
+     * @return
+     */
+    public static float calcWinkel(final PointF point, final PointF rotationCenter) {
+        final float distTCenterX = rotationCenter.x - point.x;
+        final float distTCenterY = rotationCenter.y - point.y;
+        final float alpha = (float) Math.atan(distTCenterY / distTCenterX);
+        float winkel = (float) (alpha * 180 / Math.PI);
+        if (point.x <= rotationCenter.x) {
+            winkel = winkel + 180;
+        }
+        return winkel;
+    }
 
-	/**
-	 * Returns the Winkel (0-360) between the two points
-	 *
-	 * @param center
-	 * @param location
-	 * @return
-	 */
-	public static float getDegreesToCenter(final PointF center, final PointF location) {
-		final float distTCenterX = center.x - location.x;
-		final float distTCenterY = center.y - location.y;
-		final float alpha = (float) Math.atan(distTCenterY / distTCenterX);
-		float winkel = (float) (alpha * 180 / Math.PI);
-		// Log.i("Winkel", "Winkel = " + winkel + "(" + alpha + ")");
-		if (location.x <= center.x) {
-			winkel = winkel + 180;
-		}
-		return winkel;
-	}
+    public float getIncrementingDegrees() {
+        incrementingDegrees += Settings.getIncrementingDegreesAddingAmount();
+        if (incrementingDegrees > 180) {
+            incrementingDegrees = -180 + (incrementingDegrees - 180);
+        }
+        if (incrementingDegrees < -180) {
+            incrementingDegrees = 180 - (incrementingDegrees + 180);
+        }
+        return incrementingDegrees;
+    }
 
+    public int getRandom90Degrees() {
+        if (Settings.isRandomDegreesAdding()) {
+            if (Randomizer.getRandomBoolean()) {
+                return Settings.getRandomDegreesAddingAmount();
+            }
+        }
+        return 0;
+    }
+
+    // /**
+    // * Returns the Winkel (0-360) between the two points
+    // *
+    // * @param center
+    // * @param location
+    // * @return
+    // */
+    // public static float getDegreesToCenter(final PointF rotationCenter, final PointF location) {
+    // final float distTCenterX = rotationCenter.x - location.x;
+    // final float distTCenterY = rotationCenter.y - location.y;
+    // final float alpha = (float) Math.atan(distTCenterY / distTCenterX);
+    // float winkel = (float) (alpha * 180 / Math.PI);
+    // // Log.i("Winkel", "Winkel = " + winkel + "(" + alpha + ")");
+    // if (location.x <= rotationCenter.x) {
+    // winkel = winkel + 180;
+    // }
+    // return winkel;
+    // }
 }
--- src/de/geithonline/wallpaperdesigner/shapes/SplatterPath.java
+++ src/de/geithonline/wallpaperdesigner/shapes/SplatterPath.java
@@ -14,120 +14,120 @@ import de.geithonline.wallpaperdesigner.utils.Randomizer;
 
 public class SplatterPath extends Path {
 
-	public enum SPLATTER_TYPE {
-		Cloud, Drop, Bacteria, Square, Triangle, CloudWithHoles, CircularMaze;
-	}
+    public enum SPLATTER_TYPE {
+        Cloud, Drop, Bacteria, Square, Triangle, CloudWithHoles, CircularMaze;
+    }
 
-	/**
-	 * @param ecken
-	 * @param center
-	 * @param radius
-	 * @param rotate
-	 *            0-2pi
-	 */
-	public SplatterPath(final PointF center, final float radius, final boolean filled, final SPLATTER_TYPE type) {
-		super();
+    /**
+     * @param ecken
+     * @param center
+     * @param radius
+     * @param rotate
+     *            0-2pi
+     */
+    public SplatterPath(final PointF center, final float radius, final boolean filled, final SPLATTER_TYPE type) {
+        super();
 
-		switch (type) {
-		default:
-		case Cloud:
-			drawCloud(center, radius, false);
-			break;
-		case CloudWithHoles:
-			drawCloud(center, radius, true);
-			break;
-		case Drop:
-			drawDrop(center, radius);
-			break;
-		case Bacteria:
-			drawBacteria(center, radius);
-			break;
-		case Square:
-			drawSquares(center, radius);
-			break;
-		case Triangle:
-			drawTriangle(center, radius);
-			break;
-		case CircularMaze:
-			drawCircularMaze(center, radius);
-			break;
-		}
+        switch (type) {
+            default:
+            case Cloud:
+                drawCloud(center, radius, false);
+                break;
+            case CloudWithHoles:
+                drawCloud(center, radius, true);
+                break;
+            case Drop:
+                drawDrop(center, radius);
+                break;
+            case Bacteria:
+                drawBacteria(center, radius);
+                break;
+            case Square:
+                drawSquares(center, radius);
+                break;
+            case Triangle:
+                drawTriangle(center, radius);
+                break;
+            case CircularMaze:
+                drawCircularMaze(center, radius);
+                break;
+        }
 
-	}
+    }
 
-	private void drawCloud(final PointF center, final float radius, final boolean withHoles) {
-		for (int i = 0; i < 15; i++) {
-			final float x = center.x + Randomizer.getRandomFloat(-radius * 0.7f, radius * 0.7f);
-			final float y = center.y + Randomizer.getRandomFloat(-radius * 0.7f, radius * 0.7f);
-			final float circleRadius = Randomizer.getRandomFloat(radius * 0.05f, radius * 0.5f);
-			final Path c = new CirclePath(new PointF(x, y), circleRadius, 0, true, CIRCLE_STYLE.CIRCLE);
-			if (withHoles) {
-				if (Randomizer.getRandomBooleanTrueInPercentOfCases(80)) {
-					op(c, Op.UNION);
-				} else {
-					op(c, Op.DIFFERENCE);
-				}
-			} else {
-				op(c, Op.UNION);
-			}
-		}
-	}
+    private void drawCloud(final PointF center, final float radius, final boolean withHoles) {
+        for (int i = 0; i < 15; i++) {
+            final float x = center.x + Randomizer.getRandomFloat(-radius * 0.7f, radius * 0.7f);
+            final float y = center.y + Randomizer.getRandomFloat(-radius * 0.7f, radius * 0.7f);
+            final float circleRadius = Randomizer.getRandomFloat(radius * 0.05f, radius * 0.5f);
+            final Path c = new CirclePath(new PointF(x, y), circleRadius, 0, true, CIRCLE_STYLE.CIRCLE);
+            if (withHoles) {
+                if (Randomizer.getRandomBooleanTrueInPercentOfCases(80)) {
+                    op(c, Op.UNION);
+                } else {
+                    op(c, Op.DIFFERENCE);
+                }
+            } else {
+                op(c, Op.UNION);
+            }
+        }
+    }
 
-	private void drawSquares(final PointF center, final float radius) {
-		for (int i = 0; i < 8; i++) {
-			final float x = center.x + Randomizer.getRandomFloat(-radius * 0.7f, radius * 0.7f);
-			final float y = center.y + Randomizer.getRandomFloat(-radius * 0.7f, radius * 0.7f);
-			final float circleRadius = Randomizer.getRandomFloat(radius * 0.05f, radius * 0.5f);
-			// final Path c = new CirclePath(new PointF(x, y), circleRadius, 0, true,
-			// CIRCLE_STYLE.CIRCLE);
-			final Path c = new SquarePath(new PointF(x, y), circleRadius, true, SQUARE_STYLE.NORMAL, Direction.CW);
-			op(c, Op.UNION);
-		}
-	}
+    private void drawSquares(final PointF center, final float radius) {
+        for (int i = 0; i < 8; i++) {
+            final float x = center.x + Randomizer.getRandomFloat(-radius * 0.7f, radius * 0.7f);
+            final float y = center.y + Randomizer.getRandomFloat(-radius * 0.7f, radius * 0.7f);
+            final float circleRadius = Randomizer.getRandomFloat(radius * 0.05f, radius * 0.5f);
+            // final Path c = new CirclePath(new PointF(x, y), circleRadius, 0, true,
+            // CIRCLE_STYLE.CIRCLE);
+            final Path c = new SquarePath(new PointF(x, y), circleRadius, true, SQUARE_STYLE.NORMAL, Direction.CW);
+            op(c, Op.UNION);
+        }
+    }
 
-	private void drawBacteria(final PointF center, final float radius) {
-		for (int i = 0; i < 15; i++) {
-			final float x = center.x + Randomizer.getRandomFloat(-radius / 2, radius / 2);
-			final float y = center.y + Randomizer.getRandomFloat(-radius / 2, radius / 2);
-			final float circleRadius = Randomizer.getRandomFloat(radius * 0.05f, radius * 0.5f);
-			final Path c;
-			if (Randomizer.getRandomBooleanTrueInPercentOfCases(70)) {
-				c = new VirusPath(new PointF(x, y), circleRadius, true);
-			} else {
-				c = new CirclePath(new PointF(x, y), circleRadius, 0, true, CIRCLE_STYLE.CIRCLE);
-			}
-			op(c, Op.UNION);
-		}
-	}
+    private void drawBacteria(final PointF center, final float radius) {
+        for (int i = 0; i < 15; i++) {
+            final float x = center.x + Randomizer.getRandomFloat(-radius / 2, radius / 2);
+            final float y = center.y + Randomizer.getRandomFloat(-radius / 2, radius / 2);
+            final float circleRadius = Randomizer.getRandomFloat(radius * 0.05f, radius * 0.5f);
+            final Path c;
+            if (Randomizer.getRandomBooleanTrueInPercentOfCases(70)) {
+                c = new VirusPath(new PointF(x, y), circleRadius, true);
+            } else {
+                c = new CirclePath(new PointF(x, y), circleRadius, 0, true, CIRCLE_STYLE.CIRCLE);
+            }
+            op(c, Op.UNION);
+        }
+    }
 
-	private void drawDrop(final PointF center, final float radius) {
-		final Path circle = new VirusPath(center, radius * 0.3f, true);
-		op(circle, Op.UNION);
-		for (int i = 0; i < 15; i++) {
-			final PointF c = new PointF();
-			c.x = center.x + Randomizer.getRandomFloat(-radius / 2, radius / 2);
-			c.y = center.y + Randomizer.getRandomFloat(-radius / 2, radius / 2);
-			final float circleRadius = Randomizer.getRandomFloat(radius * 0.15f, radius * 0.5f);
-			final Path path = new DropPath(c, circleRadius);
-			final float winkel = Rotator.getDegreesToCenter(center, c) - 90;
-			PathHelper.rotatePath(c, path, winkel);
-			op(path, Op.UNION);
-		}
-	}
+    private void drawDrop(final PointF center, final float radius) {
+        final Path circle = new VirusPath(center, radius * 0.3f, true);
+        op(circle, Op.UNION);
+        for (int i = 0; i < 15; i++) {
+            final PointF c = new PointF();
+            c.x = center.x + Randomizer.getRandomFloat(-radius / 2, radius / 2);
+            c.y = center.y + Randomizer.getRandomFloat(-radius / 2, radius / 2);
+            final float circleRadius = Randomizer.getRandomFloat(radius * 0.15f, radius * 0.5f);
+            final Path path = new DropPath(c, circleRadius);
+            final float winkel = Rotator.calcWinkel(c, center) - 90;
+            PathHelper.rotatePath(c, path, winkel);
+            op(path, Op.UNION);
+        }
+    }
 
-	private void drawTriangle(final PointF center, final float radius) {
-		for (int i = 0; i < 15; i++) {
-			final PointF c = new PointF();
-			c.x = center.x + Randomizer.getRandomFloat(-radius / 2, radius / 2);
-			c.y = center.y + Randomizer.getRandomFloat(-radius / 2, radius / 2);
-			final float circleRadius = Randomizer.getRandomFloat(radius * 0.15f, radius * 0.5f);
-			final Path path = new XEckPath(3, c, circleRadius, Randomizer.getRandomFloat(0, (float) Math.PI), true);
-			op(path, Op.UNION);
-		}
-	}
+    private void drawTriangle(final PointF center, final float radius) {
+        for (int i = 0; i < 15; i++) {
+            final PointF c = new PointF();
+            c.x = center.x + Randomizer.getRandomFloat(-radius / 2, radius / 2);
+            c.y = center.y + Randomizer.getRandomFloat(-radius / 2, radius / 2);
+            final float circleRadius = Randomizer.getRandomFloat(radius * 0.15f, radius * 0.5f);
+            final Path path = new XEckPath(3, c, circleRadius, Randomizer.getRandomFloat(0, (float) Math.PI), true);
+            op(path, Op.UNION);
+        }
+    }
 
-	private void drawCircularMaze(final PointF center, final float radius) {
-		addPath(new CircularMazePath(center, radius, new CircularMazeOptions(), MazeType.V1));
-	}
+    private void drawCircularMaze(final PointF center, final float radius) {
+        addPath(new CircularMazePath(center, radius, new CircularMazeOptions(), MazeType.V1));
+    }
 
 }
